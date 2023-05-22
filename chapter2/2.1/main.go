ba5e3e7a54ef6228bb6cb26bfb7d1249cb047bda
bbremer <bennettjamesbremer@gmail.com>
2023-05-21 20:07:35 -0400

    Finish 2.2.

--- chapter2/2.1/main.go
+++ chapter2/2.1/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "fmt"
-    "temperature/tempconv"
+	"fmt"
+	"temperature/tempconv"
 )
 
 func main() {
-    fmt.Printf("Brrr! %v\n", tempconv.AbsoluteZeroC)
-    fmt.Println(tempconv.CToF(tempconv.BoilingC))
-    fmt.Printf("0°C is %v.\n", tempconv.CToK(0))
-    fmt.Printf("300°K is %v\n", tempconv.KToF(300))
-    tempconv.TestF()
+	fmt.Printf("Brrr! %v\n", tempconv.AbsoluteZeroC)
+	fmt.Println(tempconv.CToF(tempconv.BoilingC))
+	fmt.Printf("0°C is %v.\n", tempconv.CToK(0))
+	fmt.Printf("300°K is %v\n", tempconv.KToF(300))
+	tempconv.TestF()
 }
--- chapter2/2.2/main.go
+++ chapter2/2.2/main.go
@@ -1,16 +1,58 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
+type celsius float64
+type fahrenheit float64
+
+type pounds float64
+type kilos float64
+
+func cToF(c celsius) fahrenheit { return fahrenheit(c*9/5 + 32) }
+func fToC(f fahrenheit) celsius { return celsius((f - 32) * 5 / 9) }
+
+func pToK(p pounds) kilos { return kilos(0.454592 * p) }
+func kToP(k kilos) pounds { return pounds(2.20462 * k) }
+
+func conversions(valueStr string) [4]string {
+	value, err := strconv.ParseFloat(valueStr, 64)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "\"%v\" is not a valid float64. Error: %s\n", valueStr, err)
+		os.Exit(1)
+	}
+
+	baseString := fmt.Sprint(value, " %v is %v %v")
+
+	cfString := fmt.Sprintf(baseString, "°C", cToF(celsius(value)), "°F")
+	fcString := fmt.Sprintf(baseString, "°F", fToC(fahrenheit(value)), "°C")
+
+	pkString := fmt.Sprintf(baseString, "lbs", pToK(pounds(value)), "kg")
+	kpString := fmt.Sprintf(baseString, "kg", kToP(kilos(value)), "lbs")
+
+	return [4]string{cfString, fcString, pkString, kpString}
+}
+
 func main() {
-    fmt.Println(os.Args)
-    var args []string
-    if len(os.Args) == 0 {
-        fmt.Println("No args. TODO: Read from STDIN.")
-    } else {
-        args = os.Args
-    }
+	var scanner *bufio.Scanner
+	if len(os.Args) == 1 {
+		scanner = bufio.NewScanner(os.Stdin)
+	} else {
+		joinedString := strings.Join(os.Args, " ")
+		scanner = bufio.NewScanner(strings.NewReader(joinedString))
+		scanner.Split(bufio.ScanWords)
+		scanner.Scan() // Get rid of program arg.
+	}
+
+	for scanner.Scan() {
+		for _, outstring := range conversions(scanner.Text()) {
+			fmt.Println(outstring)
+		}
+		fmt.Println()
+	}
 }
